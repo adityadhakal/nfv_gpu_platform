62da42dec30ccac43fb8c066799282863b881520
Aditya <dhakaladitya@Adityas-MacBook-Pro.local>
2019-10-25 14:53:21 -0700

    "testing github from VScode mac"

--- onvm/onvm_mgr/onvm_gpu.h
+++ onvm/onvm_mgr/onvm_gpu.h
@@ -8,6 +8,7 @@
 
 #include "onvm_cntk_api.h"
 
+//GPU Resource allocation flags and data structure 
 #define GPU_MAX_RA_PER_NF	(100)			// indictes max % that can be allocated to an NF
 #define MAX_GPU_OVERPRIVISION_VALUE	(200)	// indicates max % at which GPU can be over provisioned or oversubscribed.
 #define DEFAULT_GPU_RA_VALUE	(30)		// indicates the default value to use \% for models that do not have profiled data. When set, also set nf->gpu_monitor_lat to TRUE.
